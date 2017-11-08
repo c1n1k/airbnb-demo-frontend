1d772d128f94845fd3d956cdebff089aba7ccb05
Oleg <c1n1kk@gmail.com>
2017-11-08 23:16:57 +0300

    fix styles

--- src/Inner/Inner.js
+++ src/Inner/Inner.js
@@ -2,9 +2,13 @@
 import styled from "styled-components";
 
 export default styled.div`
+  position: relative;
   margin: 0 auto;
   padding: 0;
-  max-width: 962px;
+  display: flex;
   width: 100%;
+  max-width: 962px;
   height: 100%;
+  justify-content: space-between;
+  align-items: baseline;
 `;
