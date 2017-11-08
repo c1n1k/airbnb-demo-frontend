37e92aad1371738b2b60da385b799f4e0619e909
Oleg <c1n1kk@gmail.com>
2017-11-08 23:14:23 +0300

    fix logo position

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -11,7 +11,12 @@ const Header = styled.header`
   border-bottom: 1px solid rgba(72, 72, 72, 0.2);
 `;
 
-const HeaderLogo = styled.div`@supports (display: grid) {grid-column: 1 / 2;}`;
+const HeaderLogo = styled.div`
+  @supports (display: grid) {
+    grid-column: 1 / 2;
+    height: 100%;
+  }
+`;
 
 const HeaderSearch = styled.div`
   @supports (display: grid) {
