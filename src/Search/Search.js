bbb4a3665b1e4d397920136a9ab13dd5ccf8764c
Oleg <c1n1kk@gmail.com>
2017-11-08 23:13:48 +0300

    add icon

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import Input from "../Input/Input";
-// import Icon from "../Icon/Icon";
+import Icon from "../Icon/Icon";
 // import searchIcon from "./search.svg";
 
 const Search = styled.div`position: relative;`;
 
 const SearchInput = styled(Input)`padding-left: 50px;`;
 
+const SearchIcon = styled.span`
+  position: absolute;
+  top: 13px;
+  left: 16px;
+  display: inline-block;
+  opacity: 0.6;
+  pointer-events: none;
+`;
+
+const SearchIconSearch = props => {
+  return (
+    <SearchIcon>
+      <Icon icon="search" width="22" height="22" fill="#3d3d3d" />
+    </SearchIcon>
+  );
+};
+
 export default () => {
   return (
     <Search>
-      {/* <Icon icon="search" width="22" height="22" fill="#3d3d3d" /> */}
+      <SearchIconSearch />
       <SearchInput placeholder="Try Miami" />
     </Search>
   );
