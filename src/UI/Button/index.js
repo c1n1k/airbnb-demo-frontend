148f49dcf673110c8e79bf87c48b86ed4fce162c
Oleg <c1n1kk@gmail.com>
2017-11-17 15:09:48 +0300

    upd style

--- src/UI/Button/index.js
+++ src/UI/Button/index.js
@@ -3,19 +3,10 @@ import styled from "styled-components";
 
 export default styled.button`
   padding: 0 16px;
-  margin: 0 6px;
   display: inline-block;
   height: 32px;
   cursor: pointer;
   background-color: #fff;
   border: 1px solid rgba(72, 72, 72, 0.2);
   border-radius: 4px;
-
-  &:first-child {
-    margin-left: 0;
-  }
-
-  &:last-child {
-    margin-right: 0;
-  }
 `;
